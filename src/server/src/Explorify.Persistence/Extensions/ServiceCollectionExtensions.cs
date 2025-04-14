0474bdaa1c89cd9bc45ba2c9de796f4dd9eb1c6a
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-04-14 13:05:45 +0300

    Create add persistence extension method
    Create initial add persistence extension method to service collection, for registering persistence related services to the DI container

---
+++ src/server/src/Explorify.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Explorify.Persistence.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var dbConnection = configuration.GetConnectionString("DbConnection");
+
+        services.AddDbContext<ExplorifyDbContext>(options =>
+            options.UseSqlServer(dbConnection));
+
+        return services;
+    }
+}
