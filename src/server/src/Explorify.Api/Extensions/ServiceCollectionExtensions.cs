21e60a45a15fd92f71a8533aba6ea57e3b32c731
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-04-14 13:20:37 +0300

    Add two more extension methods
    Add extension method for registering all of the services in the presentation layer and also an extension for building the middleware pipeline

---
+++ src/server/src/Explorify.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Explorify.Persistence.Extensions;
+
+namespace Explorify.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddServices(
+       this IServiceCollection services,
+       IConfiguration configuration)
+    {
+        services
+            .AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+        services
+            .AddPersistence(configuration)
+            .AddSwaggerGen()
+            .AddAuthorization()
+            .AddEndpointsApiExplorer();
+
+        return services;
+    }
+}
---
+++ src/server/src/Explorify.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Explorify.Api.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static void ConfigureMiddlewarePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app
+                .UseSwagger()
+                .UseSwaggerUI();
+        }
+
+        app
+            .UseHttpsRedirection()
+            .UseAuthentication()
+            .UseAuthorization();
+
+        app.MapControllers();
+    }
+}
