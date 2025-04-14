b86f8d1915b46bc040453ae2271530df19a0b13c
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-04-14 11:36:13 +0300

    Create initial application user and role
    Create the initial application user and application role in persistence layer

---
+++ src/server/src/Explorify.Persistence/Identity/ApplicationRole.cs
@@ -0,0 +1,24 @@
+﻿using Explorify.Domain.Abstractions.Contracts;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Explorify.Persistence.Identity;
+
+public class ApplicationRole :
+    IdentityRole<Guid>,
+    IAuditInfo,
+    IDeletableEntity
+{
+    public ApplicationRole(string name)
+        : base(name)
+    {
+    }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
---
+++ src/server/src/Explorify.Persistence/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Explorify.Domain.Abstractions.Contracts;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Explorify.Persistence.Identity;
+
+public class ApplicationUser :
+    IdentityUser<Guid>,
+    IAuditInfo
+{
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+}
