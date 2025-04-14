1b67ae9bef4251aa76d89e4dae1b403ee219c20a
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-04-14 15:11:14 +0300

    Create initial database seeding logic
    Create ISeeder interface and Explorify Db Context seeder class, responsible for seeding the database

---
+++ src/server/src/Explorify.Persistence/Seeding/ExplorifyDbContextSeeder.cs
@@ -0,0 +1,20 @@
+﻿namespace Explorify.Persistence.Seeding;
+
+public class ExplorifyDbContextSeeder : ISeeder
+{
+    public async Task SeedAsync(
+        ExplorifyDbContext dbContext,
+        IServiceProvider serviceProvider)
+    {
+        ArgumentNullException.ThrowIfNull(dbContext);
+
+        IEnumerable<ISeeder> seeders = [];
+
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAsync(dbContext, serviceProvider);
+        }
+
+        await dbContext.SaveChangesAsync();
+    }
+}
---
+++ src/server/src/Explorify.Persistence/Seeding/ISeeder.cs
@@ -0,0 +1,8 @@
+﻿namespace Explorify.Persistence.Seeding;
+
+public interface ISeeder
+{
+    Task SeedAsync(
+        ExplorifyDbContext dbContext,
+        IServiceProvider serviceProvider);
+}
