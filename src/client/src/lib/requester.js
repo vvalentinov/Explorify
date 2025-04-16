558859182dbfb9f735f4824cc5382d4366de8398
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-04-16 10:58:44 +0300

    Create initial requester helper function

---
+++ src/client/src/lib/requester.js
@@ -0,0 +1,37 @@
+const request = async (method, token, url, data) => {
+
+    const options = {};
+
+    options.method = method;
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = { 'Content-Type': 'application/json' };
+    }
+
+    if (token) { options.headers = { ...options.headers, 'Authorization': `Bearer ${token}` }; }
+
+    const response = await fetch(url, options);
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        if (result.errors.length > 0) {
+            throw result.errors;
+        }
+
+        throw result;
+    }
+
+    return result?.data ?? result;
+};
+
+export const requestFactory = (token) => {
+    return {
+        get: request.bind(null, 'GET', token),
+        post: request.bind(null, 'POST', token),
+        put: request.bind(null, 'PUT', token),
+        patch: request.bind(null, 'PATCH', token),
+        delete: request.bind(null, 'DELETE', token),
+    }
+};
