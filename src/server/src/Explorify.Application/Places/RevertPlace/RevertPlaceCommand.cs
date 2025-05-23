4214fc3ec45e776489f1b7cf58501249b75508f6
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-05-23 14:17:12 +0300

    Create initial revert place command

---
+++ src/server/src/Explorify.Application/Places/RevertPlace/RevertPlaceCommand.cs
@@ -0,0 +1,8 @@
+﻿using Explorify.Application.Abstractions.Interfaces.Messaging;
+
+namespace Explorify.Application.Places.RevertPlace;
+
+public record RevertPlaceCommand(
+    Guid PlaceId,
+    Guid CurrentUserId,
+    bool IsCurrUserAdmin) : ICommand;
