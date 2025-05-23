0faa76c7bd260bb53b0c3fe35e8dc7b865d31964
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-05-23 14:17:27 +0300

    Create initial delete review command

---
+++ src/server/src/Explorify.Application/Reviews/Delete/DeleteReviewCommand.cs
@@ -0,0 +1,8 @@
+﻿using Explorify.Application.Abstractions.Interfaces.Messaging;
+
+namespace Explorify.Application.Reviews.Delete;
+
+public record DeleteReviewCommand(
+    Guid Reviewid,
+    Guid CurrentUserId,
+    bool IsCurrUserAdmin) : ICommand;
---
+++ src/server/src/Explorify.Application/Reviews/Delete/DeleteReviewCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Explorify.Domain.Entities;
+using Explorify.Application.Abstractions.Models;
+using Explorify.Application.Abstractions.Interfaces;
+using Explorify.Application.Abstractions.Interfaces.Messaging;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Explorify.Application.Reviews.Delete;
+
+public class DeleteReviewCommandHandler
+    : ICommandHandler<DeleteReviewCommand>
+{
+    private readonly IRepository _repository;
+
+    public DeleteReviewCommandHandler(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result> Handle(
+        DeleteReviewCommand request,
+        CancellationToken cancellationToken)
+    {
+        var reviewId = request.Reviewid;
+        var currUserId = request.CurrentUserId;
+        var isCurrUserAdmin = request.IsCurrUserAdmin;
+
+        var review = await _repository
+            .All<Review>()
+            .FirstOrDefaultAsync(x =>
+                x.Id == reviewId, cancellationToken);
+
+        if (review == null)
+        {
+            var error = new Error("No review found!", ErrorType.Validation);
+            return Result.Failure(error);
+        }
+
+        if (review.UserId != currUserId && !isCurrUserAdmin)
+        {
+            var error = new Error("Only review owner or admin can delete review!", ErrorType.Validation);
+            return Result.Failure(error);
+        }
+
+        _repository.SoftDelete(review);
+
+        await _repository.SaveChangesAsync();
+
+        return Result.Success("Successfully deleted review!");
+    }
+}
