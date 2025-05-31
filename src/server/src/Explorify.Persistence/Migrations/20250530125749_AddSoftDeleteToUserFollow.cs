ff61e6ca17370f3e6f86af9fb80eea815d98b752
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-05-31 09:26:25 +0300

    Add soft delete to user follow

---
+++ src/server/src/Explorify.Persistence/Migrations/20250530125749_AddSoftDeleteToUserFollow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Explorify.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteToUserFollow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "UserFollows",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "UserFollows",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserFollows",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "UserFollows",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "UserFollows");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "UserFollows");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserFollows");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "UserFollows");
+        }
+    }
+}
